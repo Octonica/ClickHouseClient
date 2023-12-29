303e717e43f2a9d1e51ec2b492f43cc184f753fe
Alec Anikin <alec.anikin@gmail.com>
2023-12-29 16:44:29 +0300

    Fix copyright year

--- src/Octonica.ClickHouseClient/Utils/ReadOnlyMemoryCollectionList.cs
+++ src/Octonica.ClickHouseClient/Utils/ReadOnlyMemoryCollectionList.cs
@@ -1,5 +1,5 @@
 ﻿#region License Apache 2.0
-/* Copyright 2021 Octonica
+/* Copyright 2023 Octonica
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
