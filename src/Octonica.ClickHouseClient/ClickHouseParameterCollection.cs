c6562adf7d51f7db0bb26c7d7103989078392d9d
Victor Sushko <victor.sushko@gmail.com>
2024-12-08 14:16:42 +0500

    Update year in the header comment (see commit d549741a910743f3eefd081df4d46bb41f523ecc)

--- src/Octonica.ClickHouseClient/ClickHouseParameterCollection.cs
+++ src/Octonica.ClickHouseClient/ClickHouseParameterCollection.cs
@@ -1,5 +1,5 @@
 ﻿#region License Apache 2.0
-/* Copyright 2019-2021 Octonica
+/* Copyright 2019-2021, 2024 Octonica
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
